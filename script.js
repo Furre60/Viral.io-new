2df22369e642c7bdb7f016d88257d50f5ce26a18
Furre60 <Adham.zaki075@gmail.com>
2024-12-05 23:33:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyDhOQ8WBGX6CgkRwyCiRhGhiCx93wz_L_c",
+    authDomain: "viral-2de41.firebaseapp.com",
+    projectId: "viral-2de41",
+    storageBucket: "viral-2de41.firebasestorage.app",
+    messagingSenderId: "1074723679254",
+    appId: "1:1074723679254:web:03445debbac201072d9937",
+    measurementId: "G-9TYGZN1SSV"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
