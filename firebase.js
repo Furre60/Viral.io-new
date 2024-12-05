e9f5098f26255ab8380118fb33e294057e47b8c7
Furre60 <Adham.zaki075@gmail.com>
2024-12-05 15:46:55 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,15 @@
-// firebase.js
-
-// Import the functions you need from the SDKs you need
+// Import the functions you need from the Firebase SDK
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDhOQ8WBGX6CgkRwyCiRhGhiCx93wz_L_c",
   authDomain: "viral-2de41.firebaseapp.com",
   projectId: "viral-2de41",
-  storageBucket: "viral-2de41.firebasestorage.app",
+  storageBucket: "viral-2de41.appspot.com",
   messagingSenderId: "1074723679254",
   appId: "1:1074723679254:web:03445debbac201072d9937",
   measurementId: "G-9TYGZN1SSV"
@@ -20,11 +17,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Firebase services
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
 const analytics = getAnalytics(app);
-
-export { auth, db, storage, analytics };
