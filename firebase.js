e9e2c5fdf4f3ef77606e0555f7f15fe864659d80
Furre60 <Adham.zaki075@gmail.com>
2024-12-05 22:41:46 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
 
