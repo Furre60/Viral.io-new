9b08db409fa4831ca253caca74ac8a86c3be2472
Furre60 <Adham.zaki075@gmail.com>
2024-12-06 18:11:42 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,29 +1,15 @@
 // firebase-config.js
 
-// Import the necessary Firebase SDKs
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-// Firebase configuration
+// Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDhOQ8WBGX6CgkRwyCiRhGhiCx93wz_L_c",
-  authDomain: "viral-2de41.firebaseapp.com",
-  projectId: "viral-2de41",
-  storageBucket: "viral-2de41.appspot.com",
-  messagingSenderId: "1074723679254",
-  appId: "1:1074723679254:web:03445debbac201072d9937",
-  measurementId: "G-9TYGZN1SSV"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase services
-const analytics = getAnalytics(app); // Google Analytics
-const auth = getAuth(app); // Firebase Authentication
-const db = getFirestore(app); // Firestore Database
-
-// Export the Firebase services to use in other files
-export { app, analytics, db, auth };
+// Export the Firebase config
+export default firebaseConfig;
