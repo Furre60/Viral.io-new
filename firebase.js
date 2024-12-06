2a9915f0f69347b2e735a78f1f877c47a4a8e154
Furre60 <Adham.zaki075@gmail.com>
2024-12-06 21:46:02 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,14 @@
 // firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-storage.js";
 
-// Your Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDhOQ8WBGX6CgkRwyCiRhGhiCx93wz_L_c",
   authDomain: "viral-2de41.firebaseapp.com",
   projectId: "viral-2de41",
-  storageBucket: "viral-2de41.firebasestorage.app",
+  storageBucket: "viral-2de41.appspot.com",
   messagingSenderId: "1074723679254",
   appId: "1:1074723679254:web:03445debbac201072d9937",
   measurementId: "G-9TYGZN1SSV"
@@ -17,8 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
 
-// Export necessary functions and objects
-export { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword };
+// Export Firebase services
+export const auth = getAuth(app);
+export const storage = getStorage(app);
