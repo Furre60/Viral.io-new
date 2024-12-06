bb69e58bcb47a055d20359749770a6b22043cdd7
Furre60 <Adham.zaki075@gmail.com>
2024-12-06 18:15:22 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,8 @@
 // firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
-
-// Your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDhOQ8WBGX6CgkRwyCiRhGhiCx93wz_L_c",
-  authDomain: "viral-2de41.firebaseapp.com",
-  projectId: "viral-2de41",
-  storageBucket: "viral-2de41.firebasestorage.app",
-  messagingSenderId: "1074723679254",
-  appId: "1:1074723679254:web:03445debbac201072d9937",
-  measurementId: "G-9TYGZN1SSV"
-};
+import firebaseConfig from './firebase-config.js';  // Import the config
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
 
-// Export necessary functions and objects
-export { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword };
+export default app;
